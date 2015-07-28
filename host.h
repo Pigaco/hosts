09114e0e867ce2b04a53644a952e0c5ecef5e6bf
Max Heisinger <mail@maximaximal.com>
2015-07-28 21:10:23 +0200

    Move extern "C" into an ifdef to be C compatible.
    References the comments by @ruabmbua in a prior commit
    (91925e14095daa8b80ea55869cac1328a6208552).

--- host.h
+++ host.h
@@ -62,16 +62,18 @@
 #define HOST_OUTPUT_POS_CONTROLS_PLAYER_15 19
 #define HOST_OUTPUT_POS_CONTROLS_PLAYER_16 20
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 /**
  * @brief Initiates the host. 
  *
  * This can be implemented if the host needs 
  * initialization.
  */
-extern "C"
-{
-    int init(int playerCount);
-}
+int init(int playerCount);
 /**
  * @brief Gets the state of the specified button. 
  *
@@ -82,132 +84,120 @@ extern "C"
  * not be any delays. The best way to accomodate this is to use an internal
  * caching mechanism.
  */
-extern "C"
-{
-    int getButtonState(int playerID, int button);
-}
+int getButtonState(int playerID, int button);
 
-namespace piga
-{
-    class GameInput;
-}
-extern "C"
-{
-    void setGameInput(piga::GameInput *gameInput);
-}
+class GameInput;
+void setGameInput(piga::GameInput *gameInput);
 
-extern "C"
-{
-    /**
-     * @brief A shortcut to leave out the implementation of the output functions. 
-     *
-     * @return HOST_RETURNCODE_FALSE if the host doesn't implement outputs, HOST_RETURNCODE_TRUE if it does. 
-     */
-    int implementsOutputs();
-    /**
-     * @brief Returns the number of outputs (all types and positions) this host is capable of. 
-     *
-     * @return The number of outputs. Numbers can be used to iterate over the possible outputs in the getOutputType() and getOutputPos() functions.
-     */
-    int getOutputCount();
-    /**
-     * @brief Returns the type of the specified output.
-     *
-     * @param outputID The ID of the output.
-     *
-     * @return The HOST_OUTPUT_TYPE of the output. 
-     */
-    int getOutputType(int outputID);
-    /**
-     * @brief Returns the position of the specified output.
-     *
-     * @param outputID The ID of the output.
-     *
-     * @return The HOST_OUTPUT_POS of the specified output. 
-     */
-    int getOutputPos(int outputID);
-    /**
-     * @brief Returns the name of the specified output. 
-     *
-     * @param outputID The ID of the output to get the name from. 
-     *
-     * @return The name of the output. 
-     */
-    const char* getOutputName(int outputID);
-    /**
-     * @brief Returns the description of the specified output. 
-     *
-     * @param outputID The ID of the output to get the description from. 
-     *
-     * @return The description of the output. 
-     */
-    const char* getOutputDescription(int outputID);
+/**
+ * @brief A shortcut to leave out the implementation of the output functions. 
+ *
+ * @return HOST_RETURNCODE_FALSE if the host doesn't implement outputs, HOST_RETURNCODE_TRUE if it does. 
+ */
+int implementsOutputs();
+/**
+ * @brief Returns the number of outputs (all types and positions) this host is capable of. 
+ *
+ * @return The number of outputs. Numbers can be used to iterate over the possible outputs in the getOutputType() and getOutputPos() functions.
+ */
+int getOutputCount();
+/**
+ * @brief Returns the type of the specified output.
+ *
+ * @param outputID The ID of the output.
+ *
+ * @return The HOST_OUTPUT_TYPE of the output. 
+ */
+int getOutputType(int outputID);
+/**
+ * @brief Returns the position of the specified output.
+ *
+ * @param outputID The ID of the output.
+ *
+ * @return The HOST_OUTPUT_POS of the specified output. 
+ */
+int getOutputPos(int outputID);
+/**
+ * @brief Returns the name of the specified output. 
+ *
+ * @param outputID The ID of the output to get the name from. 
+ *
+ * @return The name of the output. 
+ */
+const char* getOutputName(int outputID);
+/**
+ * @brief Returns the description of the specified output. 
+ *
+ * @param outputID The ID of the output to get the description from. 
+ *
+ * @return The description of the output. 
+ */
+const char* getOutputDescription(int outputID);
 
-    int getIntOutputRangeMin(int outputID);
-    int getIntOutputRangeMax(int outputID);
-    float getFloatOutputRangeMin(int outputID);
-    float getFloatOutputRangeMax(int outputID);
-    double getDoubleOutputRangeMin(int outputID);
-    double getDoubleOutputRangeMax(int outputID);
+int getIntOutputRangeMin(int outputID);
+int getIntOutputRangeMax(int outputID);
+float getFloatOutputRangeMin(int outputID);
+float getFloatOutputRangeMax(int outputID);
+double getDoubleOutputRangeMin(int outputID);
+double getDoubleOutputRangeMax(int outputID);
 
-    /**
-     * @brief Sets a color output. 
-     *
-     * @param outputID The ID of the output. 
-     * @param r Red
-     * @param g Green
-     * @param b Blue
-     * @param a Alpha (if used)
-     *
-     * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
-     */
-    int setColorOutput(int outputID, float r, float g, float b, float a);
-    /**
-     * @brief Sets a true/false output. 
-     *
-     * @param outputID The ID of the output. 
-     * @param value 1 means true, 0 means false. 
-     *
-     * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
-     */
-    int setBoolOutput(int outputID, int value);
-    /**
-     * @brief Sets a string output. 
-     *
-     * @param outputID The ID of the output. 
-     * @param outString The string to be sent to the output. 
-     *
-     * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
-     */
-    int setStringOutput(int outputID, const char* outString);
-    /**
-     * @brief Sets an integer output. 
-     *
-     * @param outputID The ID of the output. 
-     * @param value The value to be sent to the output. 
-     *
-     * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
-     */
-    int setIntOutput(int outputID, int value);
-    /**
-     * @brief Sets a float output. 
-     *
-     * @param outputID The ID of the output. 
-     * @param value The value to be sent to the output. 
-     *
-     * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
-     */
-    int setFloatOutput(int outputID, float value);
-    /**
-     * @brief Sets a double output. 
-     *
-     * @param outputID The ID of the output. 
-     * @param value The value to be set to the output. 
-     *
-     * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
-     */
-    int setDoubleOutput(int outputID, double value);
-}
+/**
+ * @brief Sets a color output. 
+ *
+ * @param outputID The ID of the output. 
+ * @param r Red
+ * @param g Green
+ * @param b Blue
+ * @param a Alpha (if used)
+ *
+ * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
+ */
+int setColorOutput(int outputID, float r, float g, float b, float a);
+/**
+ * @brief Sets a true/false output. 
+ *
+ * @param outputID The ID of the output. 
+ * @param value 1 means true, 0 means false. 
+ *
+ * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
+ */
+int setBoolOutput(int outputID, int value);
+/**
+ * @brief Sets a string output. 
+ *
+ * @param outputID The ID of the output. 
+ * @param outString The string to be sent to the output. 
+ *
+ * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
+ */
+int setStringOutput(int outputID, const char* outString);
+/**
+ * @brief Sets an integer output. 
+ *
+ * @param outputID The ID of the output. 
+ * @param value The value to be sent to the output. 
+ *
+ * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
+ */
+int setIntOutput(int outputID, int value);
+/**
+ * @brief Sets a float output. 
+ *
+ * @param outputID The ID of the output. 
+ * @param value The value to be sent to the output. 
+ *
+ * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
+ */
+int setFloatOutput(int outputID, float value);
+/**
+ * @brief Sets a double output. 
+ *
+ * @param outputID The ID of the output. 
+ * @param value The value to be set to the output. 
+ *
+ * @return HOST_RETURNCODE_SUCCESS if successful, HOST_RETURNCODE_FAIL or HOST_RETURNCODE_FAIL_INVALID_TYPE otherwise. 
+ */
+int setDoubleOutput(int outputID, double value);
 
 /**
  * @brief Sets a callback function to be called when an input happens.
@@ -215,47 +205,42 @@ extern "C"
  * This function will only be called when @ref HOST_RETURNCODE_USECALLBACK
  * is set.
  */
-extern "C"
-{
-    void setCallbackFunc(void (*inputHappened)(int, int, int));
-}
+void setCallbackFunc(void (*inputHappened)(int, int, int));
 
 /**
  * @brief Gets called before the host will be destroyed. 
  *
  * This can be implemented if the host needs a destruction handling.
  */
-extern "C"
-{
-    int destroy();
-}
+int destroy();
 
-extern "C"
+int getPigaMajorVersion()
 {
-    int getPigaMajorVersion()
-    {
-        return HOST_VERSION_MAJOR;
-    }
-    int getPigaMinorVersion()
-    {
-        return HOST_VERSION_MINOR;
-    }
-    int getPigaMiniVersion()
-    {
-        return HOST_VERSION_MINI;
-    }
-    /**
-     * Returns the name of the host. (Set by the host programmer.)
-     */
-    const char* getName();
-    /**
-     * Returns the description of the host. (Set by the host programmer.)
-     */
-    const char* getDescription();
-    /**
-     * @brief Returns the author of the host. (Set by the host programmer.)
-     */
-    const char* getAuthor();
+    return HOST_VERSION_MAJOR;
 }
+int getPigaMinorVersion()
+{
+    return HOST_VERSION_MINOR;
+}
+int getPigaMiniVersion()
+{
+    return HOST_VERSION_MINI;
+}
+/**
+ * Returns the name of the host. (Set by the host programmer.)
+ */
+const char* getName();
+/**
+ * Returns the description of the host. (Set by the host programmer.)
+ */
+const char* getDescription();
+/**
+ * @brief Returns the author of the host. (Set by the host programmer.)
+ */
+const char* getAuthor();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
