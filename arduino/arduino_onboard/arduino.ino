ec8bd08fc3d33a4e0cc39b63a4a95a7961e57a81
Max Heisinger <mail@maximaximal.com>
2015-12-08 01:36:51 +0100

    Edit the arduino makefile to include correct baud rate.

--- arduino/arduino_onboard/arduino.ino
+++ arduino/arduino_onboard/arduino.ino
@@ -1,11 +1,71 @@
-const int hello = 0;
+#include "serial_connector.h"
+#include "button.h"
+#include "led.h"
+
+//If you change the baud rate here, you also have 
+//to change it in the makefile.
+SerialConnector serialConnector(115200);
+
+//Workaround for an arduino compiler bug
+void* __dso_handle;
+void* __cxa_atexit;
+
+//LEDs
+LED *leds[6];
+
+//Buttons
+Button *buttons[11];
 
 void setup(void)
 {
-    //Setup
+    serialConnector.setup();
+
+    Button::serialConnector = &serialConnector;
+
+    //Create LEDs and buttons.
+    //Be cautious with the IDs and pins!
+    //
+    //!!!!!!!!!!!
+    //CHANGE THIS
+    //!!!!!!!!!!!
+    
+    //LEDs use the analog pins.
+    leds[0] = new LED(0);
+    leds[1] = new LED(1);
+    leds[2] = new LED(2);
+    leds[3] = new LED(3);
+    leds[4] = new LED(4);
+    leds[5] = new LED(5);
+
+    //Buttons use the digital pins.
+    buttons[0] = new Button(2, BUTTON_UP);
+    buttons[1] = new Button(3, BUTTON_DOWN);
+    buttons[2] = new Button(4, BUTTON_LEFT);
+    buttons[3] = new Button(5, BUTTON_RIGHT);
+    buttons[4] = new Button(6, BUTTON_ACTION);
+    buttons[5] = new Button(7, BUTTON_BUTTON1);
+    buttons[6] = new Button(8, BUTTON_BUTTON2);
+    buttons[7] = new Button(9, BUTTON_BUTTON3);
+    buttons[8] = new Button(10, BUTTON_BUTTON4);
+    buttons[9] = new Button(11, BUTTON_BUTTON5);
+    buttons[10] = new Button(12, BUTTON_BUTTON6);
+
+    serialConnector.leds = leds;
 }
 
 void loop(void)
 {
-    //Loop
+    int i;
+
+    serialConnector.loop();
+
+    for(i = 0; i < 6; ++i)
+    {
+        leds[i]->loop();
+    }
+
+    for(i = 0; i < 11; ++i)
+    {
+        buttons[i]->loop();
+    }
 }
