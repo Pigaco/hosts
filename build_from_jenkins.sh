ce56a84b9d362acc69c98a00a7ee2ef4a1815a51
Max Heisinger <mail@maximaximal.com>
2016-01-18 00:12:41 +0100

    Add build_from_jenkins file.

---
+++ build_from_jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/opt/chroots/debian_jessie_x32/mount_jenkins.sh
+
+echo "Build Hosts Package"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "mkdir -p /root/jenkins/workspace/libhosts/build"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "rm -f /root/jenkins/workspace/libhosts/build/piga-libhosts-dev-*"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/libhosts/build && cmake .. -DHOSTS_VERSION_PATCH="\""$(cat /var/lib/jenkins/jobs/libhosts/nextBuildNumber)"\"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/libhosts/build && make package"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/libhosts/build && make install"
+
+echo "Add the package to the repository"
+PIGA_DEB="$(ls /var/lib/jenkins/workspace/libhosts/build/*deb)"
+echo "DEBFILE: $PIGA_DEB"
+cd /media/maximaximal.com/www/repos/apt/debian && sudo reprepro includedeb jessie "$PIGA_DEB"
+
+/opt/chroots/debian_jessie_x32/umount_jenkins.sh
